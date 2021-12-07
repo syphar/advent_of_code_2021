76c19c48709950c57d4fd18de6e1bd6a68007358
Denis Cornehl <denis@cornehl.org>
2021-12-07 22:11:47 +0100

    d06 / 1 quick fix

---
+++ d06/src/main.rs
@@ -0,0 +1,39 @@
+use std::fs;
+fn main() {
+    let input: Vec<u16> = fs::read_to_string("input.txt")
+        .unwrap()
+        .split(',')
+        .map(str::trim)
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    println!("part 1: {}", part_1(&input, 80));
+}
+
+fn part_1(input: &[u16], days: u16) -> usize {
+    let mut fish: Vec<u16> = input.to_vec();
+    for _ in 1..=days {
+        for i in 0..fish.len() {
+            if fish[i] == 0 {
+                fish.push(8);
+                fish[i] = 6;
+            } else {
+                fish[i] -= 1;
+            }
+        }
+    }
+
+    fish.len()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static NUMBERS: [u16; 5] = [3, 4, 3, 1, 2];
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(part_1(&NUMBERS, 80), 5934);
+    }
+}
